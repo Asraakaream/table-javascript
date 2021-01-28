f548ed27749cdfade282a120b3c0f0abfc2f14be
unknown <saa627375@gmail.com>
2021-01-28 14:10:56 +0300

    Iam not complete becuse the maneger want it quickly

---
+++ login.js
@@ -0,0 +1,8 @@
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (username == "asraa" && password == "asraa") {
+        
+      alert("Name must be filled out");
+      return false;
+    }
+  }
